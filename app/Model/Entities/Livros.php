afa21ba546584af95aa04ebc92c140cab7bbf67c
eloysam <114229411+eloysam@users.noreply.github.com>
2023-11-19 16:05:11 -0300

    inseri valores na tabela Livros

---
+++ app/Model/Entities/Livros.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace Ifba\Model\Entities;
+
+    class Livros{
+       private int $isbnLivro;
+       private int $preco_aluguel;
+       private int $preco_compra;
+       private string $idioma;
+       private string $genero;
+       private string $classificacao;
+       private string $nome;
+       private string $autor;
+       private string $editora;
+       private string $servico;
+    }
