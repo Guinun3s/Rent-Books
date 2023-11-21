c57e7e430a0ee4e7a8b0d9560163c8bd8ce311a2
eloysam <114229411+eloysam@users.noreply.github.com>
2023-11-20 22:53:01 -0300

    Valores na tabela Hq

---
+++ app/Model/Entities/Hq.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Ifba\Model\Entities;
+
+    class Hq{
+        private int $isbnHq;
+        private float $preco_aluguel;
+        private float $preco_compra;
+        private string $classficacao;
+        private string $editora;
+        private string $nome;
+        private string $servico;
+    }
