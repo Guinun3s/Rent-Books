1302de9ebbccf246d5da9fa50b95256e1e39e447
eloysam <114229411+eloysam@users.noreply.github.com>
2023-11-24 21:54:14 -0300

    Mexi no DataBase

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -6,6 +6,7 @@ namespace Rentbooks\Core;
 class Database{
 
     protected \PDO $conexao;
+    protected \PDOStatement $stmt;
 
     public function __construct(){
         $servidor = 'localhost';
@@ -27,26 +28,5 @@ class Database{
         return $this->stmt->fetchAll();
     }
 
-    public function executar(string $sql, array $dados = []){
-        $this->stmt = $this->conexao->prepare($sql);
-    }
-
-
 }
 
-/*
-        public function executar(string $sql, array $dados = []){
-
-        # declaracao em inglês - statime
-        $this->stmt = $this->conexao->prepare($sql); // PREPARE -> é uma substituição dos valores que estavam(?), com os que nós inserimos
-        $resultado = $this->stmt->execute($dados); # Aqui, os dados(variável) estarão indo para o stmt.
-        return $resultado;
-
-    }
-
-    public function retornaTodos(){
-
-        return $this->stmt->fetchAll();
-    }
-
-*/
