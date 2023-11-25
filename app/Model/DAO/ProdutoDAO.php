5d4fa2c3cdb300dd5e8b2edd96e95e71f126d40c
eloysam <114229411+eloysam@users.noreply.github.com>
2023-11-24 22:13:18 -0300

    Criei o ProdutoDAO

---
+++ app/Model/DAO/ProdutoDAO.php
@@ -0,0 +1,30 @@
+<?php
+
+    namespace Rentbooks\Model\DAO;
+
+use Rentbooks\Core\Database;
+
+    class ProdutoDAO{
+
+        public function inserir($produto){
+
+            $sql = "INSERT INTO Produto(idProduto, codFornecedor, codLivros)";
+        
+            $dados = [
+                $produto->idProduto,
+                $produto->codFornecedor,
+                $produto->codLivro
+            ];
+
+            $banco = new Database; // Aqui estpu crianco um banco que está em Database
+            return $banco->executar($sql, $dados);
+        }
+
+        public function buscarTodos(){
+            $sql = "SELECT * FROM Produto";
+
+            $banco = new Database;
+            $banco->executar($sql);
+            return $banco->retornaTodos();
+        }
+    }
